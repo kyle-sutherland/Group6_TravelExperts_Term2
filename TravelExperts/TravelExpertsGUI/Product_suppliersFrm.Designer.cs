02da2a98f77b5e8e2f511c1aca589d9486da87e8
Tim Huynh <timhuynh12@gmail.com>
2023-01-30 17:35:40 -0700

    Made Product_Suppliers Form

---
+++ TravelExperts/TravelExpertsGUI/Product_suppliersFrm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TravelExpertsGUI
+{
+    partial class Product_suppliersFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProdSupData = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdSupData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProdSupData
+            // 
+            this.dgvProdSupData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdSupData.Location = new System.Drawing.Point(14, 16);
+            this.dgvProdSupData.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvProdSupData.Name = "dgvProdSupData";
+            this.dgvProdSupData.RowTemplate.Height = 25;
+            this.dgvProdSupData.Size = new System.Drawing.Size(396, 425);
+            this.dgvProdSupData.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(474, 37);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(114, 47);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEdit.Location = new System.Drawing.Point(474, 122);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(114, 47);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRemove.Location = new System.Drawing.Point(474, 208);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(114, 47);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Location = new System.Drawing.Point(474, 383);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(114, 47);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // Product_suppliersFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 453);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvProdSupData);
+            this.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Product_suppliersFrm";
+            this.Text = "Product Suppliers Data";
+            this.Load += new System.EventHandler(this.Product_suppliersFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdSupData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dgvProdSupData;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnRemove;
+        private Button btnClose;
+    }
+}
---
+++ TravelExperts/TravelExpertsGUI/Product_suppliersFrm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TravelExpertsData;
+
+namespace TravelExpertsGUI
+{
+    public partial class Product_suppliersFrm : Form
+    {
+        public Product_suppliersFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Product_suppliersFrm_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                // grab data from db
+                dgvProdSupData.Columns.Clear();
+                var data = db.ProductsSuppliers
+                    .OrderBy(p => p.ProductSupplierId)
+                    .Select(p => new {p.ProductSupplierId, p.ProductId,p.SupplierId})
+                    .ToList();
+                dgvProdSupData.DataSource = data; // display
+
+                // format odd numbered rows
+                dgvProdSupData.AlternatingRowsDefaultCellStyle.BackColor = Color.PaleGoldenrod;
+                
+                dgvProdSupData.Columns[0].Width = 132; // format first column
+                dgvProdSupData.Columns[1].Width = 90; // format 2nd column
+            }
+        }
+    }
+}
