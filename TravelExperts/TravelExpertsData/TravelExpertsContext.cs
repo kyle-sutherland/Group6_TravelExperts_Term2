54bbde077db8d28d44462a92a3455c11e87c777a
kaysuh <mynameiskylesutherland@gmail.com>
2023-02-01 11:15:08 -0700

    j

--- TravelExperts/TravelExpertsData/TravelExpertsContext.cs
+++ TravelExperts/TravelExpertsData/TravelExpertsContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -40,8 +41,7 @@ namespace TravelExpertsData
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True");
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["TravelExperts"].ConnectionString);
             }
         }
 
