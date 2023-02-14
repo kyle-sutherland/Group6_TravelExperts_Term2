1ef518fefd4f5d21408e2d5453f3987833b7b7a2
kaysuh <mynameiskylesutherland@gmail.com>
2023-02-14 16:08:49 -0700

    created manager classes

--- TravelExpertsWebApp/TravelExpertsData/BookingDetailManager.cs
+++ TravelExpertsWebApp/TravelExpertsData/BookingDetailManager.cs
@@ -2,5 +2,23 @@
 {
     internal class BookingDetailManager
     {
+        public static List<BookingDetail> GetAllBookingDetail(TravelExpertsContext db)
+        {
+            List<BookingDetail> bookingDetails = db.BookingDetails.ToList();
+            return bookingDetails;
+        }
+
+        public static BookingDetail GetBookingDetailById(TravelExpertsContext db, int id)
+        {
+            BookingDetail bookingDetail = db.BookingDetails.Find(id);
+            return bookingDetail;
+        }
+
+        public static void CreateNewBookingDetail(TravelExpertsContext db, BookingDetail bookingDetail)
+        {
+            db.BookingDetails.Add(bookingDetail);
+            db.SaveChanges();
+        }
+
     }
 }
--- TravelExpertsWebApp/TravelExpertsData/BookingManager.cs
+++ TravelExpertsWebApp/TravelExpertsData/BookingManager.cs
@@ -2,6 +2,22 @@
 {
     public class BookingManager
     {
+        public static List<Booking> GetBookings(TravelExpertsContext db)
+        {
+            List<Booking> bookings = db.Bookings.ToList();
+            return bookings;
+        }
 
+        public static Booking GetBookingById(TravelExpertsContext db, int id)
+        {
+            Booking booking = db.Bookings.Find(id);
+            return booking;
+        }
+
+        public static void CreateNewBooking(TravelExpertsContext db, Booking booking)
+        {
+            db.Bookings.Add(booking);
+            db.SaveChanges();
+        }
     }
 }
